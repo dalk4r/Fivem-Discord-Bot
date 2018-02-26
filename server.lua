68c2ef56a05f940f119b69bd61f704be0ffa23a9
dalk4r <36829699+dalk4r@users.noreply.github.com>
2018-02-25 22:36:44 -0300

    Delete server.lua

--- server.lua
+++
@@ -1,3 +0,0 @@
-	AddEventHandler('chatMessage', function(source, name, message)
-  PerformHttpRequest('INSERT DISCORD WEBHOOK HERE', function(err, text, headers) end, 'POST', json.encode({username = name, content = message}), { ['Content-Type'] = 'application/json' })
-end)
