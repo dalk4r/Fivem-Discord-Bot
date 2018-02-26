27986bc01c5d40b985e8e807737ae79a381657fe
dalk4r <36829699+dalk4r@users.noreply.github.com>
2018-02-25 22:34:36 -0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,3 +1,3 @@
 	AddEventHandler('chatMessage', function(source, name, message)
-  PerformHttpRequest('INSERT DISCORD WEBHOOK HERE', function(err, text, headers) end, 'POST', json.encode({username = name, content = message}), { ['Content-Type'] = 'application/json' })
+  PerformHttpRequest('https://discordapp.com/api/webhooks/417492171545903115/ECOhQ-J_XDzoFSJAwj1-DEkAPr2I2tn_4dJ654XRs6Wzq49O3gDhJuLYfjn0Jjdx-9NB', function(err, text, headers) end, 'POST', json.encode({username = name, content = message}), { ['Content-Type'] = 'application/json' })
 end)
